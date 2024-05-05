d975a4898d9531c455ee8cf201f85d38338005c1
yamirito <yamirlopez2001@gmail.com>
2024-05-05 23:41:11 +0200

    Intentando actualizar para vercel

--- db/seed.ts
+++ db/seed.ts
@@ -2,19 +2,32 @@ import { db , producto } from 'astro:db';
 
 // https://astro.build/db/seed
 
+/*
 export default async function seed(){
 	await db.insert(producto).values([
-		{nombreProducto:"almadrullo",
-			descripcion:"nunca sabre que es eso",
-			precio:"12",
-			img:"foto",
-			tipo:"periferico",
-			autor:"cevix20000",
-			estado:"nuevo"}
+		{nombreProducto:'almadrullo',
+			descripcion:'nunca sabre que es eso',
+			precio:'12',
+			img:'foto',
+			tipo:'periferico',
+			autor:'cevix20000',
+			estado:'nuevo'
+		}
 	])
 }
+*/
 
-/*
 export default async function seed() {
 	// TODO
-}*/
+	await db.insert(producto).values([
+		{nombreProducto:'almadrullo',
+			descripcion:'nunca sabre que es eso',
+			precio:'12',
+			img:'foto',
+			tipo:'periferico',
+			autor:'cevix20000',
+			estado:'nuevo'
+		}
+	])
+
+}
